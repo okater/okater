cec488e0d5bfccfe839acf746588b189a1867357
okater <okater@gmail.com>
2020-04-11 19:45:00 +0300

    contacts and backend mail

---
+++ submit.php
@@ -0,0 +1,21 @@
+<?php
+
+$to = 'omkater@gmail.com';
+
+$subject = 'Website Change Request';
+
+$headers = "From: " . strip_tags($_POST['email']) . "\r\n";
+$headers .= "Reply-To: ". strip_tags($_POST['email']) . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=utf-8\r\n";
+
+$message = '<html><body>';
+#$message .= '<img src="//css-tricks.com/examples/WebsiteChangeRequestForm/images/wcrf-header.png" alt="Website Change Request" />';
+$message .= '<table rules="all" style="border-color: #666;" cellpadding="10">';
+$message .= "<tr style='background: #eee;'><td><strong>Name:</strong> </td><td>" . strip_tags($_POST['name']) . "</td></tr>";
+$message .= "<tr><td><strong>Email:</strong> </td><td>" . strip_tags($_POST['email']) . "</td></tr>";
+$message .= "</table>";
+$message .= '<strong>Message:</strong> ' .strip_tags($_POST(['message']));
+$message .= "</body></html>";
+
+?>
